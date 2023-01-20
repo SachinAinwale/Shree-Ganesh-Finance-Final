86b82ce672017f6f016a574dd2ee96c5901b6ace
ashutoshbhoyar <ashutoshbhoyar100@gmail.com>
2023-01-20 12:34:34 +0530

    genreation type auto genrated

--- Shree_Ganesh_Finance/src/main/java/com/shreeganesh/loan/app/customerEntities/CustomerAddress.java
+++ Shree_Ganesh_Finance/src/main/java/com/shreeganesh/loan/app/customerEntities/CustomerAddress.java
@@ -2,8 +2,12 @@ package com.shreeganesh.loan.app.customerEntities;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GeneratorType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomerAddress {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer  customerAddressId;
 	@OneToOne(cascade = CascadeType.ALL)
 	private PermanentAddress permanentAddress;
