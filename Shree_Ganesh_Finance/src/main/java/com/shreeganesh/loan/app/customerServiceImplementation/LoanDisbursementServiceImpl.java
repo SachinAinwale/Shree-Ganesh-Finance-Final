3247b3f44dab41fac49b1b362912a1cb239973d2
SachinAinwale <SachinAinwale>
2023-01-21 12:56:31 +0530

    Loan Disbursment api done

--- Shree_Ganesh_Finance/src/main/java/com/shreeganesh/loan/app/customerServiceImplementation/LoanDisbursementServiceImpl.java
+++ Shree_Ganesh_Finance/src/main/java/com/shreeganesh/loan/app/customerServiceImplementation/LoanDisbursementServiceImpl.java
@@ -14,14 +14,18 @@ import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
 import com.shreeganesh.loan.app.customerEntities.CustomerDetails;
+import com.shreeganesh.loan.app.customerEntities.Enquiry;
 import com.shreeganesh.loan.app.customerEntities.LoanDisbursement;
+import com.shreeganesh.loan.app.customerEnum.CustomerStatus;
 import com.shreeganesh.loan.app.customerRepository.CustomerRepository;
+import com.shreeganesh.loan.app.customerRepository.EnquiryRepository;
 import com.shreeganesh.loan.app.customerRepository.LoanDisbursementRepository;
 import com.shreeganesh.loan.app.customerServiceInterfaces.LoanDisbursementService;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.Date;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.mail.internet.MimeMessage;
@@ -36,86 +40,109 @@ public class LoanDisbursementServiceImpl implements LoanDisbursementService {
 	LoanDisbursement loan;
 
 	@Autowired
-	LoanDisbursementRepository ldr;
-
-	@Autowired
-	CustomerRepository custrepo;
+	CustomerRepository customerRepository;
 
 	@Autowired
 	JavaMailSender sender;
+	
+	@Autowired
+	EnquiryRepository enquiryRepository;
 
 	private Logger logger = LoggerFactory.getLogger(LoanDisbursement.class);
 
 	@Override
-	public LoanDisbursement insertData(CustomerDetails customer) {
+	public CustomerDetails insertData(Integer customerid) {
 
-		loan.setAgreementId(ThreadLocalRandom.current().nextInt(999, 9999));
-		loan.setTotalLoanAmount(customer.getCustomerSanctionLetter().getLoanAmountSanctioned());
-		loan.setDealerBankDetails(customer.getCustomerDealer().getDealerBankDetails());
-		// loan.setDealerAccountNumber(customer.getCustomerDealer().getDealerBankDetails().getDealerBankAccountNo());
-		Date date = new Date();
+		Optional<CustomerDetails> optional = customerRepository.findById(customerid);
 
-		loan.setAmountPaidDate(date);
+		if (optional.isPresent()) {
 
-		customer.setCustomerloandisbursement(loan);
-		custrepo.save(customer);
+			CustomerDetails customer = optional.get();
 
-		logger.info("Loan Disbursement PDF started");
+			Date date = new Date();
 
-		String title = "Shree Ganesh Finace";
-		String content1 = "mr" + customer.getCustomerDealer().getDealerName();
-		String content2 = "as per your vehicle quatation provided to" + customer.getCustomerFirstName() + " "
-				+ customer.getCustomerLastName() + "And further application aproved loan amount "
-				+ customer.getCustomerSanctionLetter().getLoanAmountSanctioned()
-				+ "is sanctiond by shree ganeh finace and amount is tranfer to your account"
-				+ customer.getCustomerDealer().getDealerBankDetails();
-		String content3 = "Thanks for chossing shree Ganesh Finace, hope your expiriance is pleasant";
+			customer.getCustomerloandisbursement().setLoanNo(ThreadLocalRandom.current().nextLong(999, 9999));
+			customer.getCustomerloandisbursement()
+					.setTotalLoanAmount(customer.getCustomerSanctionLetter().getLoanAmountSanctioned());
+			customer.getCustomerloandisbursement()
+					.setTransferAmount(customer.getCustomerSanctionLetter().getLoanAmountSanctioned());
+			customer.getCustomerloandisbursement()
+					.setDealerBankDetails(customer.getCustomerDealer().getDealerBankDetails());
+			customer.getCustomerloandisbursement().setPaymentStatus(String.valueOf(CustomerStatus.loandisbursed));
+			customer.setCustomerStatus(String.valueOf(CustomerStatus.loandisbursed));
+			customer.getCustomerloandisbursement().setAmountPaidDate(date);
 
-		ByteArrayOutputStream opt = new ByteArrayOutputStream();
+			customerRepository.save(customer);
+			
+			Optional<Enquiry> optional1 = enquiryRepository.findById(customer.getCustomerId());
+			if (optional.isPresent()) {
+				Enquiry enquiry = optional1.get();
+				enquiry.setEnquiryStatus(String.valueOf(CustomerStatus.loandisbursed));
+				enquiryRepository.save(enquiry);
 
-		Document document = new Document();
+			}
 
-		PdfWriter.getInstance(document, opt);
-		document.open();
 
-		Font titlefont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 25);
-		Paragraph titlepara = new Paragraph(title, titlefont);
-
-		titlepara.setAlignment(Element.ALIGN_CENTER);
-		document.add(titlepara);
-
-		Paragraph paracontent1 = new Paragraph(content1);
-		document.add(paracontent1);
-
-		Paragraph paracontent2 = new Paragraph(content2);
-		document.add(paracontent2);
-
-		Paragraph paracontent3 = new Paragraph(content3);
-		document.add(paracontent3);
-
-		document.close();
-		ByteArrayInputStream byt = new ByteArrayInputStream(opt.toByteArray());
-
-		MimeMessage mimemessage = sender.createMimeMessage();
-
-		try {
-			MimeMessageHelper mimemessageHelper = new MimeMessageHelper(mimemessage, true);
-			mimemessageHelper.setFrom("navinash111@gmail.com");
-			mimemessageHelper.setTo(customer.getCustomerDealer().getDealerEmail());
-			mimemessageHelper.setSubject("Shree Ganesh Finance regarding to Car loan");
-			mimemessageHelper.setText(
-					"Dear Dealer, We have recived sanction letter & happy to let you that we will get sanction amount");
-			byte[] bytearray = byt.readAllBytes();
-
-			mimemessageHelper.addAttachment("loanDisbursmentletter.pdf", new ByteArrayResource(bytearray));
-			sender.send(mimemessage);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			logger.info("Loan Disbursement PDF started");
+//
+//			String title = "Shree Ganesh Finace";
+//			String content1 = "mr" + customer.getCustomerDealer().getDealerName();
+//			String content2 = "as per your vehicle quatation provided to" + customer.getCustomerFirstName() + " "
+//					+ customer.getCustomerLastName() + "And further application aproved loan amount "
+//					+ customer.getCustomerSanctionLetter().getLoanAmountSanctioned()
+//					+ "is sanctiond by shree ganeh finace and amount is tranfer to your account"
+//					+ customer.getCustomerDealer().getDealerBankDetails();
+//			String content3 = "Thanks for chossing shree Ganesh Finace, hope your expiriance is pleasant";
+//
+//			ByteArrayOutputStream opt = new ByteArrayOutputStream();
+//
+//			Document document = new Document();
+//
+//			PdfWriter.getInstance(document, opt);
+//			document.open();
+//
+//			Font titlefont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 25);
+//			Paragraph titlepara = new Paragraph(title, titlefont);
+//
+//			titlepara.setAlignment(Element.ALIGN_CENTER);
+//			document.add(titlepara);
+//
+//			Paragraph paracontent1 = new Paragraph(content1);
+//			document.add(paracontent1);
+//
+//			Paragraph paracontent2 = new Paragraph(content2);
+//			document.add(paracontent2);
+//
+//			Paragraph paracontent3 = new Paragraph(content3);
+//			document.add(paracontent3);
+//
+//			document.close();
+//			ByteArrayInputStream byt = new ByteArrayInputStream(opt.toByteArray());
+//
+//			MimeMessage mimemessage = sender.createMimeMessage();
+//
+//			try {
+//				MimeMessageHelper mimemessageHelper = new MimeMessageHelper(mimemessage, true);
+//				mimemessageHelper.setFrom("navinash111@gmail.com");
+//				mimemessageHelper.setTo(customer.getCustomerDealer().getDealerEmail());
+//				mimemessageHelper.setSubject("Shree Ganesh Finance regarding to Car loan");
+//				mimemessageHelper.setText(
+//						"Dear Dealer, We have recived sanction letter & happy to let you that we will get sanction amount");
+//				byte[] bytearray = byt.readAllBytes();
+//
+//				mimemessageHelper.addAttachment("loanDisbursmentletter.pdf", new ByteArrayResource(bytearray));
+//				sender.send(mimemessage);
+//
+//			} catch (Exception e) {
+//				
+//				e.printStackTrace();
+//			}
+			return customer;
 		}
-
-		return ldr.save(loan);
+		else {
+			return null;
+		}
+		
 	}
 
 }
